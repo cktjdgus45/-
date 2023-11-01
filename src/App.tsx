8afa291040691fd23dee7dec0f2577563c9c6ea9
pooh <cktjdgus45@naver.com>
2023-11-01 20:05:09 +0900

    display my device coords

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { NavermapsProvider } from 'react-naver-maps';
 import MyMap from './components/MyMap.tsx';
 
 function App() {
-  console.log(process.env.REACT_APP_NAVER_Client_ID);
   return (
     <NavermapsProvider
       ncpClientId={process.env.REACT_APP_NAVER_Client_ID! as string}
--- src/components/MyMap.tsx
+++ src/components/MyMap.tsx
@@ -1,27 +1,120 @@
 import React from 'react';
-import { Container as MapDiv, NaverMap, Marker, useNavermaps } from 'react-naver-maps'
+import { useState, useEffect, useRef } from 'react';
+import { Container as MapDiv, NaverMap, useNavermaps, InfoWindow, useListener, Overlay, Listener } from 'react-naver-maps';
 
-const MyMap = () => {
+export default function MyMap() {
     const navermaps = useNavermaps()
+
+    // 마커를 한번만 생성하기 위해 useState lazy initialize 사용
+    const [marker1] = useState(
+        () =>
+            new navermaps.Marker({
+                position: { lat: 37.2186019, lng: 126.9536571 },
+            }),
+    )
+    // 마커를 한번만 생성하기 위해 useRef 사용
+    const marker2Ref = useRef<naver.maps.Marker | null>(null);
+    if (!marker2Ref.current) {
+        marker2Ref.current = new navermaps.Marker({
+            position: { lat: 37.2186019, lng: 126.9536571 },
+        });
+    }
+    const marker2 = marker2Ref.current
+    const [map, setMap] = useState<naver.maps.Map | null>(null)
+    const [infowindow, setInfoWindow] = useState<naver.maps.InfoWindow | null>(null)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    function onSuccessGeolocation(position) {
+        if (!map || !infowindow) return
+
+        const location = new navermaps.LatLng(
+            position.coords.latitude,
+            position.coords.longitude,
+        )
+        map.setCenter(location);
+        map.setZoom(10);
+        infowindow.setContent(
+            '<div style="padding:10px;">' +
+            `${location.toString()}` +
+            '</div>',
+        )
+        infowindow.setPosition(location);
+        infowindow.open(map, location) //정보창을 엽니다.
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    function onErrorGeolocation() {
+        if (!map || !infowindow) return
+
+        const center = map.getCenter()
+        infowindow.setContent(
+            '<div style="padding:20px;">' +
+            '<h5 style="margin-bottom:5px;color:#f00;">Geolocation failed!</h5>' +
+            'latitude: ' +
+            center.x +
+            '<br />longitude: ' +
+            center.y +
+            '</div>',
+        )
+        infowindow.open(map, center)
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                onSuccessGeolocation,
+                onErrorGeolocation,
+            )
+        } else {
+            const center = map.getCenter()
+            infowindow.setContent(
+                '<div style="padding:20px;"><h5 style="margin-bottom:5px;color:#f00;">Geolocation not supported</h5></div>',
+            )
+            infowindow.open(map, center)
+        }
+    }
+    useListener(marker1, 'click', () => window.alert('서울시청 click'))
+    useEffect(() => {
+        if (!map || !infowindow) {
+            return
+        }
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                onSuccessGeolocation,
+                onErrorGeolocation,
+            )
+        } else {
+            var center = map.getCenter()
+            infowindow.setContent(
+                '<div style="padding:20px;"><h5 style="margin-bottom:5px;color:#f00;">Geolocation not supported</h5></div>',
+            )
+            infowindow.open(map, center)
+        }
+    }, [map, infowindow, onSuccessGeolocation, onErrorGeolocation])
+
     return (
         <MapDiv
             style={{
+                position: 'relative',
                 width: '100%',
                 height: '600px',
             }}
         >
             <NaverMap
-                defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
-                defaultZoom={15}
+                defaultCenter={new navermaps.LatLng(37.5666805, 126.9784147)}
+                defaultZoom={10}
+                defaultMapTypeId={navermaps.MapTypeId.NORMAL}
+                ref={setMap}
             >
-                <Marker
-                    defaultPosition={new navermaps.LatLng(37.3595704, 127.105399)}
-                />
+                <InfoWindow ref={setInfoWindow} content={'내 위치'} />
+                <Overlay element={marker1} />
+                <Overlay element={marker2}>
+                    {/* Component 로 이벤트 리스너 등록 */}
+                    <Listener
+                        type="click"
+                        listener={() => window.alert('popo click')}
+                    />
+                </Overlay>
             </NaverMap>
-
         </MapDiv>
-    );
-};
+    )
+}
 
-
-export default MyMap;
