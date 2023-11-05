bb34c29828eff430d39c2203a4cd956ea5988c23
pooh <cktjdgus45@naver.com>
2023-11-05 20:52:14 +0900

    add Hospital type

--- src/data/latlon.ts
+++ src/data/latlon.ts
@@ -3,13 +3,41 @@ export type Coord = {
     lng: number;
 }
 
+export type Hospital = {
+    name: string;
+    phone: string;
+    jibunAdress: string;
+    coord: Coord;
+}
+
 export const coords: Coord[] = [
     {
-        lat: 37.223583,
-        lng: 126.952858,
+        lat: 37.223313,
+        lng: 126.952805
     },
     {
-        lat: 37.224768,
-        lng: 126.947382
+        lat: 37.224726,
+        lng: 126.947334
     },
 ];
+
+export const hospitals: Hospital[] = [
+    {
+        name: 'a병원',
+        phone: '010-0111-1111',
+        jibunAdress: '경기도 화성시 봉담읍 10-35',
+        coord: {
+            lat: 37.223313,
+            lng: 126.952805
+        }
+    },
+    {
+        name: 'b병원',
+        phone: '010-0000-2222',
+        jibunAdress: '경기도 화성시 봉담읍 11-10',
+        coord: {
+            lat: 37.224726,
+            lng: 126.947334
+        }
+    },
+]
