a1a1e28e93b31bf9f6742ab360c832afae2a1738
pooh <cktjdgus45@naver.com>
2024-01-20 19:18:46 +0900

    add my profile page

---
+++ src/components/profile/Hero.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Avartar from '../UI/Avartar.tsx';
+import { IAuthHandler, IUser } from '../../types';
+
+interface IHero {
+    authHandler: IAuthHandler;
+    setEditProfileForm: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Hero = ({ authHandler, setEditProfileForm }: IHero) => {
+    const { username, name, url } = authHandler.user?.user! as IUser;
+    return (
+        <div className='flex items-center p-6'>
+            <Avartar width={28} height={28} url={url} username={username} />
+            <div className='ml-5 text-base font-bold text-main-color'>
+                <div className="flex items-center mb-3">
+                    <h3 className='mr-5 font-normal'>{username}</h3>
+                    <button className='transition-colors duration-200 hover:text-white ease-in-out text-hover-main-color p-2 rounded-sm bg-main-color' onClick={() => setEditProfileForm(true)}>Edit Profile</button>
+                </div>
+                <h3>{name}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,17 +3,20 @@ import { useParams } from 'react-router-dom';
 import { IPostServiceProps } from '../App';
 import UpdateProfileForm from '../components/community/UpdateProfileForm.tsx';
 import { useAuth } from '../context/AuthContext.tsx';
+import Hero from '../components/profile/Hero.tsx';
 
 const Profile = ({ postService }: IPostServiceProps) => {
-    const { username } = useParams();
+    const { username } = useParams(); // get user's posts with postService
     const authHandler = useAuth();
     const [editProfileForm, setEditProfileForm] = useState(false);
-    console.log(username);
     return (
-        <>
-            <button onClick={() => setEditProfileForm(true)}>Edit Profile</button>
+        <div className='w-full h-full '>
+            <Hero authHandler={authHandler} setEditProfileForm={setEditProfileForm} />
+            <div className='w-full h-full bg-slate-500'>
+                posts
+            </div>
             {editProfileForm && (<UpdateProfileForm setEditProfileForm={setEditProfileForm} authHandler={authHandler} />)}
-        </>
+        </div>
     )
 }
 
