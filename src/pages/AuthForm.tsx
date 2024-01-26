d3cd79b8d6a124bab6dbf0e7cb6007bc557f4aca
pooh <cktjdgus45@naver.com>
2024-01-26 21:36:48 +0900

    change AuthForm Css

--- src/pages/AuthForm.tsx
+++ src/pages/AuthForm.tsx
@@ -61,13 +61,13 @@ const AuthForm = ({ onSignUp, onLogin }) => {
   return (
     <div className='flex flex-col justify-start items-center h-screen'>
       <div className='relative w-full h-full'>
-        <video src={dogVideo} autoPlay loop muted controls={false} />
+        <video className='object-cover w-full h-full' src={dogVideo} autoPlay loop muted controls={false} />
         <Banner text={text} isAlert={isAlert} />
         <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-[rgba(0,0,0,0.4)]'>
-          <div className='p-5 bg-glass flex flex-col justify-center items-center rounded-lg shadow-lg'>
+          <div className='p-5 bg-glass flex flex-col justify-center items-center rounded-lg shadow-2xl'>
             <nav className='text-white text-base font-semibold flex self-start justify-self-start gap-4'>
-              <h3 onClick={() => setSignup(!signup)} className={`${signup ? 'border-b-2' : ''} border-cyan-300 cursor-pointer`}>회원가입</h3>
-              <h3 onClick={() => setSignup(!signup)} className={`${signup ? '' : 'border-b-2'} border-pink-300 cursor-pointer`}>로그인</h3>
+              <h3 onClick={() => setSignup(!signup)} className={`${signup ? 'border-b-2' : ''} border-slate-700 cursor-pointer`}>회원가입</h3>
+              <h3 onClick={() => setSignup(!signup)} className={`${signup ? '' : 'border-b-2'} border-stone-700 cursor-pointer`}>로그인</h3>
             </nav>
             <form className='flex flex-col items-center justify-center mt-2' onSubmit={onSubmit}>
               <input
@@ -76,7 +76,7 @@ const AuthForm = ({ onSignUp, onLogin }) => {
                 placeholder='Id'
                 value={username}
                 onChange={onChange}
-                className={`transition-colors duration-200 ease-in-out px-4 py-2 text-sm text-neutral-50 font-light mb-1 bg-glass-md focus:bg-glass ${signup ? 'focus:border-cyan-300' : 'focus:border-pink-300'} ${signup ? 'hover:border-cyan-50' : 'hover:border-pink-50'} border-glass border-[0.7px] border-solid placeholder:text-[rgba(0,0,0,0.6)]`}
+                className={`placeholder:font-semibold outline-none transition-colors duration-200 ease-in-out px-4 py-2 text-sm text-neutral-50 font-light mb-1 bg-glass-md focus:outline-none hover:outline-none focus:bg-glass ${signup ? 'focus:border-slate-700' : 'focus:border-stone-700'} ${signup ? 'hover:border-slate-50' : 'hover:border-stone-50'} border-glass !important border-2 border-solid placeholder:text-[rgba(255,255,255,0.9)]`}
                 required
               />
               <input
@@ -84,7 +84,7 @@ const AuthForm = ({ onSignUp, onLogin }) => {
                 type='password'
                 placeholder='Password'
                 value={password}
-                className={`transition-colors duration-200 ease-in-out px-4 py-2 text-sm text-neutral-50  font-light mb-1 bg-glass-md focus:bg-glass ${signup ? 'focus:border-cyan-300' : 'focus:border-pink-300'} ${signup ? 'hover:border-cyan-50' : 'hover:border-pink-50'} border-glass border-[0.7px] border-solid placeholder:text-[rgba(0,0,0,0.6)]`}
+                className={`placeholder:font-semibold outline-none transition-colors duration-200 ease-in-out px-4 py-2 text-sm text-neutral-50  font-light mb-1 bg-glass-md focus:outline-none hover:outline-none focus:bg-glass ${signup ? 'focus:border-slate-700' : 'focus:border-stone-700'} ${signup ? 'hover:border-slate-50' : 'hover:border-stone-50'} border-glass !important border-2 border-solid placeholder:text-[rgba(255,255,255,0.9)]`}
                 onChange={onChange}
               />
               {signup && (
@@ -94,7 +94,7 @@ const AuthForm = ({ onSignUp, onLogin }) => {
                   placeholder='이름'
                   value={name}
                   onChange={onChange}
-                  className={`transition-colors duration-200 ease-in-out px-4 py-2 text-sm text-neutral-50  font-light mb-1 bg-glass-md focus:bg-glass ${signup ? 'focus:border-cyan-300' : 'focus:border-pink-300'} ${signup ? 'hover:border-cyan-50' : 'hover:border-pink-50'} border-glass border-[0.7px] border-solid placeholder:text-[rgba(0,0,0,0.6)]`}
+                  className={`placeholder:font-semibold outline-none transition-colors duration-200 ease-in-out px-4 py-2 text-sm text-neutral-50  font-light mb-1 bg-glass-md focus:outline-none hover:outline-none focus:bg-glass ${signup ? 'focus:border-slate-700' : 'focus:border-stone-700'} ${signup ? 'hover:border-slate-50' : 'hover:border-stone-50'} border-glass !important border-2 border-solid placeholder:text-[rgba(255,255,255,0.9)]`}
                   required
                 />
               )}
@@ -105,7 +105,7 @@ const AuthForm = ({ onSignUp, onLogin }) => {
                   placeholder='이메일'
                   value={email}
                   onChange={onChange}
-                  className={`transition-colors duration-200 ease-in-out px-4 py-2 text-sm text-neutral-50  font-light mb-1 bg-glass-md focus:bg-glass ${signup ? 'focus:border-cyan-300' : 'focus:border-pink-300'} ${signup ? 'hover:border-cyan-50' : 'hover:border-pink-50'} border-glass border-[0.7px] border-solid placeholder:text-[rgba(0,0,0,0.6)]`}
+                  className={`placeholder:font-semibold outline-none transition-colors duration-200 ease-in-out px-4 py-2 text-sm text-neutral-50  font-light mb-1 bg-glass-md focus:outline-none hover:outline-none focus:bg-glass ${signup ? 'focus:border-slate-700' : 'focus:border-stone-700'} ${signup ? 'hover:border-slate-50' : 'hover:border-stone-50'} border-glass !important border-2 border-solid placeholder:text-[rgba(255,255,255,0.9)]`}
                   required
                 />
               )}
@@ -116,10 +116,10 @@ const AuthForm = ({ onSignUp, onLogin }) => {
                   placeholder='프로필 사진URL(선택)'
                   value={url}
                   onChange={onChange}
-                  className={`transition-colors duration-200 ease-in-out px-4 py-2 text-sm text-neutral-50 font-light mb-1 bg-glass-md focus:bg-glass ${signup ? 'focus:border-cyan-300' : 'focus:border-pink-300'} ${signup ? 'hover:border-cyan-50' : 'hover:border-pink-50'} border-glass border-[0.7px] border-solid placeholder:text-[rgba(0,0,0,0.6)]`}
+                  className={`placeholder:font-semibold outline-none transition-colors duration-200 ease-in-out px-4 py-2 text-sm text-neutral-50 font-light mb-1 bg-glass-md focus:outline-none hover:outline-none focus:bg-glass ${signup ? 'focus:border-slate-700' : 'focus:border-stone-700'} ${signup ? 'hover:border-slate-50' : 'hover:border-stone-50'} border-glass !important border-2 border-solid placeholder:text-[rgba(255,255,255,0.9)]`}
                 />
               )}
-              <button className={`tracking-wide transition-colors duration-300 ease-in-out self-end px-1 py-2 mt-3 rounded-lg text-sm text-white font-semibold ${signup ? 'bg-cyan-300' : 'bg-pink-300'} ${signup ? 'hover:bg-cyan-500' : 'hover:bg-pink-500'}`} type='submit'>
+              <button className={`tracking-wide transition-colors duration-700 ease-in-out self-end px-1 py-2 mt-3 rounded-lg text-sm text-white font-semibold ${signup ? 'bg-slate-300' : 'bg-stone-300'} ${signup ? 'hover:bg-slate-700' : 'hover:bg-stone-700'}`} type='submit'>
                 {loading ? (
                   <Loader kind='clip' isLoading={loading} color='#fff' />
                 ) : (
