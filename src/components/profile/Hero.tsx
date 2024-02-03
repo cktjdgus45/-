58e630dfbf5844c85357c6ddb92a277dcdea4be6
pooh <cktjdgus45@naver.com>
2024-02-03 19:21:14 +0900

    change Hero css

--- src/components/profile/Hero.tsx
+++ src/components/profile/Hero.tsx
@@ -10,12 +10,12 @@ interface IHero {
 const Hero = ({ authHandler, setEditProfileForm }: IHero) => {
     const { username, name, url } = authHandler.user?.user! as IUser;
     return (
-        <div className='flex items-center p-6'>
+        <div className='flex gap-3 items-center p-6'>
             <Avartar width={28} height={28} url={url} name={username} />
-            <div className='ml-5 text-base font-bold text-main-color'>
-                <div className="flex items-center mb-3">
+            <div className='text-sm font-semibold text-span-color'>
+                <div className="flex gap-3 items-center mb-3 text-span-color">
                     <h3>{name}</h3>
-                    <button className='transition-colors duration-200 hover:text-white ease-in-out text-hover-main-color p-2 rounded-sm bg-main-color' onClick={() => setEditProfileForm(true)}>Edit Profile</button>
+                    <button className="transition-colors duration-200 hover:text-white ease-in-out text-hover-main-color p-2 rounded-full bg-main-color" onClick={() => setEditProfileForm(true)}>Edit Profile</button>
                 </div>
                 <h3 className='mr-5 font-normal'>{username}</h3>
             </div>
